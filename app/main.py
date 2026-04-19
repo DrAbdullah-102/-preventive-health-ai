89d57824c6024a02fbe75ee1fa53e262394f6ac4
DrAbdullah-102 <almasriabdullah102@gmail.com>
2026-04-19 21:06:54 +0200

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,6 @@
+symptoms = input("Enter symptoms: ")
+
+if "fever" in symptoms and "cough" in symptoms:
+    print("Possible viral infection. Rest and hydration recommended.")
+else:
+    print("Please consult a doctor for more information.")
